1ec0bd41c78cc17497d99a1a13a968fa83c77a2f
Hanxiong Shi <hanxiong@havenlife.com>
2022-12-07 21:06:34 -0500

    feat: init application layout

--- src/app.py
+++ src/app.py
@@ -5,33 +5,49 @@ from windows import set_dpi_awareness
 # set high resolution in windows 10
 set_dpi_awareness()
 
-
+# app config
 root = tk.Tk()
-root.title("Greeter")
+root.title("Video Loom")
+root.geometry("800x1000")
 
-user_name = tk.StringVar()
-
-main = ttk.Frame(root, padding=(20, 10, 20, 0))
-main.grid()
-
+# app layout
 root.columnconfigure(0, weight=1)
-root.rowconfigure(1, weight=1)
+root.rowconfigure(0, weight=20)
+root.rowconfigure(1, weight=6)
+root.rowconfigure(2, weight=1)
 
-name_label = ttk.Label(main, text="Name:")
-name_label.grid(row=0, column=0, padx=(0, 10))
-name_entry = ttk.Entry(main, width=15, textvariable=user_name)
-name_entry.grid(row=0, column=1)
-name_entry.focus()
 
-buttons = ttk.Frame(root, padding=(20, 10))
-buttons.grid(row=1, column=0, sticky="EW")
+# videos input
+video_component = ttk.Frame(root, padding=(10, 0))
+video_component.grid(row=0, sticky="EW")
 
-buttons.columnconfigure(0, weight=1)
-buttons.columnconfigure(1, weight=1)
+video_label = ttk.Label(video_component, text="Videos").pack()
 
-greet_button = ttk.Button(buttons, text="Greet")
-greet_button.grid(row=0, column=0, sticky="EW")
-quit_button = ttk.Button(buttons, text="Quit", command=root.destroy)
+
+# timeline
+timeline_component = ttk.Frame(root, padding=(10, 0))
+timeline_component.grid(row=1, sticky="EW")
+
+timeline_label = ttk.Label(timeline_component, text="Timeline").pack()
+
+timeline_text = tk.Text(timeline_component, height=20).pack()
+
+
+# toolbar
+toolbar_component = ttk.Frame(root, padding=(10, 0))
+toolbar_component.grid(row=2, sticky="EW")
+
+toolbar_component.columnconfigure(0, weight=1)
+toolbar_component.columnconfigure(1, weight=1)
+
+generate_button = ttk.Button(
+    toolbar_component, text="Generate Video", padding=(10))
+generate_button.grid(row=0, column=0, sticky="EW")
+
+quit_button = ttk.Button(toolbar_component, text="Quit",
+                         padding=(10), command=root.destroy)
 quit_button.grid(row=0, column=1, sticky="EW")
 
+
+# app execution
 root.mainloop()
