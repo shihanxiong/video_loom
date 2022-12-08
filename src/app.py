2d18958a69c77ec5fa41e9123e685fd6baf07928
Hanxiong Shi <hanxiong@havenlife.com>
2022-12-07 21:42:10 -0500

    feat: remove .pack()

--- src/app.py
+++ src/app.py
@@ -9,33 +9,60 @@ set_dpi_awareness()
 root = tk.Tk()
 root.title("Video Loom")
 root.geometry("800x1000")
+root.resizable(False, False)
+
 
 # app layout
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=20)
-root.rowconfigure(1, weight=6)
-root.rowconfigure(2, weight=1)
+root.rowconfigure(1, weight=3)
+root.rowconfigure(2, weight=6)
+root.rowconfigure(3, weight=1)
 
 
 # videos input
 video_component = ttk.Frame(root, padding=(10, 0))
-video_component.grid(row=0, sticky="EW")
+video_component.grid(row=0, sticky="N")
+video_component.rowconfigure(0, weight=0)
+video_component.rowconfigure(1, weight=1)
 
-video_label = ttk.Label(video_component, text="Videos").pack()
+video_label = ttk.Label(video_component, text="Videos")
+video_label.grid(row=0)
+
+
+# audio setting
+audio_setting_component = ttk.Frame(root, padding=(10, 0))
+audio_setting_component.grid(row=1, sticky="N")
+audio_setting_component.rowconfigure(0, weight=0)
+audio_setting_component.rowconfigure(1, weight=1)
+
+audio_setting_label = ttk.Label(
+    audio_setting_component, text="Audio Settings")
+audio_setting_label.grid(row=0)
 
 
 # timeline
-timeline_component = ttk.Frame(root, padding=(10, 0))
-timeline_component.grid(row=1, sticky="EW")
+timeline_component = ttk.Frame(root, padding=(10, 10))
+timeline_component.grid(row=2, sticky="SEW")
+timeline_component.rowconfigure(0, weight=0)
+timeline_component.rowconfigure(1, weight=1)
+timeline_component.columnconfigure(0, weight=1)
+timeline_component.columnconfigure(1, weight=0)
 
-timeline_label = ttk.Label(timeline_component, text="Timeline").pack()
+timeline_label = ttk.Label(timeline_component, text="Timeline")
+timeline_label.grid(row=0)
 
-timeline_text = tk.Text(timeline_component, height=20).pack()
+timeline_text = tk.Text(timeline_component, height=25)
+timeline_text.grid(row=1, column=0)
+
+text_scroll = ttk.Scrollbar(
+    timeline_component, orient="vertical", command=timeline_text.yview)
+text_scroll.grid(row=1, column=1, sticky="NS")
 
 
 # toolbar
 toolbar_component = ttk.Frame(root, padding=(10, 0))
-toolbar_component.grid(row=2, sticky="EW")
+toolbar_component.grid(row=3, sticky="NEW")
 
 toolbar_component.columnconfigure(0, weight=1)
 toolbar_component.columnconfigure(1, weight=1)
