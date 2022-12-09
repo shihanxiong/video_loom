89e8a160751fd491fce63e90f749e4f5b641b32d
Hanxiong Shi <hanxiong@havenlife.com>
2022-12-08 21:49:36 -0500

    wife commit

--- src/app.py
+++ src/app.py
@@ -1,9 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
 from windows import set_dpi_awareness
+
 
 # set high resolution in windows 10
 set_dpi_awareness()
+
+
+# methods
+def select_file():
+    filetypes = (
+        ('video files', '*.mp4'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    if filename != "":
+        video_list.append(filename)
+        showinfo(
+            title='Selected File',
+            message=filename
+        )
+        print(video_list)
+        video_label.config(text=f"Videos {len(video_list)} of 2")
+
 
 # app config
 root = tk.Tk()
@@ -15,9 +41,14 @@ root.resizable(False, False)
 # app layout
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=20)
-root.rowconfigure(1, weight=3)
-root.rowconfigure(2, weight=6)
+root.rowconfigure(1, weight=0)
+root.rowconfigure(2, weight=1)
 root.rowconfigure(3, weight=1)
+
+
+# variables
+video_list = []
+audio_track_variable = tk.IntVar()
 
 
 # videos input
@@ -26,19 +57,33 @@ video_component.grid(row=0, sticky="N")
 video_component.rowconfigure(0, weight=0)
 video_component.rowconfigure(1, weight=1)
 
-video_label = ttk.Label(video_component, text="Videos")
+video_label = ttk.Label(
+    video_component, text=f"Videos {len(video_list)} of 2")
 video_label.grid(row=0)
+
+video_import_button = ttk.Button(video_component, text="Import a video",
+                                 padding=(10), command=select_file)
+video_import_button.grid(row=1)
 
 
 # audio setting
-audio_setting_component = ttk.Frame(root, padding=(10, 0))
-audio_setting_component.grid(row=1, sticky="N")
+audio_setting_component = ttk.Frame(root, padding=(20, 0))
+audio_setting_component.grid(row=1, sticky="NEW")
 audio_setting_component.rowconfigure(0, weight=0)
 audio_setting_component.rowconfigure(1, weight=1)
+audio_setting_component.columnconfigure(0, weight=1)
+audio_setting_component.columnconfigure(1, weight=1)
 
 audio_setting_label = ttk.Label(
     audio_setting_component, text="Audio Settings")
-audio_setting_label.grid(row=0)
+audio_setting_label.grid(row=0, columnspan=3)
+
+audio_track_option_1 = ttk.Radiobutton(
+    audio_setting_component, text="Audio track 1", variable=audio_track_variable, value=0)
+audio_track_option_2 = ttk.Radiobutton(
+    audio_setting_component, text="Audio track 2", variable=audio_track_variable, value=1)
+audio_track_option_1.grid(row=1, column=0, sticky="N")
+audio_track_option_2.grid(row=1, column=1, sticky="N")
 
 
 # timeline
@@ -52,7 +97,7 @@ timeline_component.columnconfigure(1, weight=0)
 timeline_label = ttk.Label(timeline_component, text="Timeline")
 timeline_label.grid(row=0)
 
-timeline_text = tk.Text(timeline_component, height=25)
+timeline_text = tk.Text(timeline_component, height=16)
 timeline_text.grid(row=1, column=0)
 
 text_scroll = ttk.Scrollbar(
