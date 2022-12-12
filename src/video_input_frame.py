a353f65f4dcf3ef620eece874a994a4895514c8c
Hanxiong Shi <hanxiong@havenlife.com>
2022-12-12 00:00:33 -0500

    feat: output video now supports audio

--- src/video_input_frame.py
+++ src/video_input_frame.py
@@ -57,11 +57,15 @@ class VideoInputFrame(ttk.Frame):
             os.remove(self.output_file_name.get())
 
         # video processing
-        stream = ffmpeg.input(os.path.abspath(self.video_list[0]))
-        stream = ffmpeg.hflip(stream)
-        stream = ffmpeg.output(stream, self.output_file_name.get())
+        pts = "PTS-STARTPTS"
         try:
-            ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
+            stream = ffmpeg.input(os.path.abspath(self.video_list[0]))
+            video = ffmpeg.hflip(stream)
+            audio = stream.filter_("asetpts", pts)
+            video_and_audio = ffmpeg.concat(video, audio, v=1, a=1)
+            output = ffmpeg.output(
+                video_and_audio, self.output_file_name.get(), format="mp4")
+            ffmpeg.run(output, capture_stdout=True, capture_stderr=True)
         except ffmpeg.Error as e:
             print("stdout:", e.stdout.decode("utf8"))
             print("stderr:", e.stderr.decode('utf8'))
