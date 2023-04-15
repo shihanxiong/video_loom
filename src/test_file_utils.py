c915938407dcec8f25c0172082d945d3157d6343
Hanxiong Shi <hanxiong@havenlife.com>
2023-04-15 12:21:49 -0400

    fix: fixed test

--- src/test_file_utils.py
+++ src/test_file_utils.py
@@ -10,4 +10,4 @@ def test_escape_file_name():
 def test_get_latest_version_from_changelog():
     file_utils = FileUtils()
     assert bool(re.match('^[v][0-9][.][0-9][.][0-9]$', file_utils.get_latest_version_from_changelog())
-                ) or file_utils.get_latest_version_from_changelog() == 'unreleased' or file_utils.get_latest_version_from_changelog() == 'Hackday'
+                ) or file_utils.get_latest_version_from_changelog() == 'Unreleased' or file_utils.get_latest_version_from_changelog() == 'Hackday'
