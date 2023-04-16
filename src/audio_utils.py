25bd19845889269ad56567af65e74aa4b94e4cdd
Hanxiong Shi <hanxiong@havenlife.com>
2023-04-16 00:17:39 -0400

    refactor: code clean up

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -11,5 +11,15 @@ class AudioUtils():
         # using preset = ultrafast as the audio is for preview only
         cmd = f"ffmpeg -i {mp4_input_filename} -vn -preset ultrafast -acodec mp3 {mp3_output_filename}"
         subprocess.check_output(cmd, shell=True)
+        # TODO: do logging via status_component instead
         logging.debug(
             f"generated {mp3_output_filename} from video {mp4_input_filename}")
+
+        return mp3_output_filename
+
+    @staticmethod
+    def generate_aac_from_mp4(mp4_input_filename, aac_output_filename, ffmpeg_preset_arg):
+        cmd = f"ffmpeg -i {mp4_input_filename} -vn -acodec copy {ffmpeg_preset_arg} {aac_output_filename}"
+        subprocess.check_output(cmd, shell=True)
+
+        return aac_output_filename
