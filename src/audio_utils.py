bfe874ccadc4e60b240a50dd653f628f6ba2de1b
Hanxiong Shi <shihanxiong@gmail.com>
2023-04-16 22:39:15 -0400

    fix: fixed the audio file escape name issue (#75)
    * fix: fixed the audio file escape name issue
    
    * fix: fixed file name escape issue
    
    * feat: generated v1.5.1 executable for win32
    
    ---------
    
    Co-authored-by: Hanxiong Shi <hanxiong@havenlife.com>

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -1,5 +1,6 @@
 import subprocess
 import logging
+from file_utils import FileUtils
 
 
 class AudioUtils():
@@ -9,7 +10,7 @@ class AudioUtils():
     @staticmethod
     def generate_mp3_from_mp4(mp4_input_filename, mp3_output_filename, ffmpeg_preset_arg="-preset ultrafast"):
         # using preset = ultrafast as the audio is for preview only
-        cmd = f"ffmpeg -i {mp4_input_filename} -vn -preset ultrafast -acodec mp3 {ffmpeg_preset_arg} {mp3_output_filename}"
+        cmd = f"ffmpeg -i {mp4_input_filename} -vn -preset ultrafast -acodec mp3 {ffmpeg_preset_arg} {FileUtils.escape_file_name(mp3_output_filename)}"
         subprocess.check_output(cmd, shell=True)
         # TODO: do logging via status_component instead
         logging.debug(
@@ -19,7 +20,7 @@ class AudioUtils():
 
     @staticmethod
     def generate_aac_from_mp4(mp4_input_filename, aac_output_filename, ffmpeg_preset_arg):
-        cmd = f"ffmpeg -i {mp4_input_filename} -vn -acodec copy {ffmpeg_preset_arg} {aac_output_filename}"
+        cmd = f"ffmpeg -i {mp4_input_filename} -vn -acodec copy {ffmpeg_preset_arg} {FileUtils.escape_file_name(aac_output_filename)}"
         subprocess.check_output(cmd, shell=True)
 
         return aac_output_filename
