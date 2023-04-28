46cc6987fa9cfdf7337ad6fc8aaf548e1fd7c2dd
Hanxiong Shi <shihanxiong@gmail.com>
2023-04-28 09:03:11 -0400

    Update test_video_utils.py (#93)
    fix: fixed a typo

--- src/test_video_utils.py
+++ src/test_video_utils.py
@@ -6,6 +6,6 @@ def test_get_ffmpeg_preset_value_for_nvenc_h264():
     assert VideoUtils.get_ffmpeg_preset_value_for_nvenc_h264("veryslow") == "slow"
     assert VideoUtils.get_ffmpeg_preset_value_for_nvenc_h264("medium") == "medium"
     assert (
-        VideoUtils.get_ffmpeg_preset_value_for_nvenc_h264("not-a-valie-preset")
+        VideoUtils.get_ffmpeg_preset_value_for_nvenc_h264("not-a-valid-preset")
         == "medium"
     )
