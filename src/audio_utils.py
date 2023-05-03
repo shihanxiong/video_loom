1b97be64a631065085356c02d998d48804186b86
Hanxiong Shi <hanxiong@havenlife.com>
2023-05-03 17:34:02 -0400

    fix: fixed the issue when audio.aac is shorter than output.mp4

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -3,24 +3,29 @@ import logging
 from file_utils import FileUtils
 
 
-class AudioUtils():
+class AudioUtils:
     def __init__(self):
         pass
 
     @staticmethod
-    def generate_mp3_from_mp4(mp4_input_filename, mp3_output_filename, ffmpeg_preset_arg="-preset ultrafast"):
+    def generate_mp3_from_mp4(
+        mp4_input_filename, mp3_output_filename, ffmpeg_preset_arg="-preset ultrafast"
+    ):
         # using preset = ultrafast as the audio is for preview only
         cmd = f"ffmpeg -i {mp4_input_filename} -vn -preset ultrafast -acodec mp3 {ffmpeg_preset_arg} {FileUtils.escape_file_name(mp3_output_filename)}"
         subprocess.check_output(cmd, shell=True)
         # TODO: do logging via status_component instead
         logging.debug(
-            f"generated {mp3_output_filename} from video {mp4_input_filename}")
+            f"generated {mp3_output_filename} from video {mp4_input_filename}"
+        )
 
         return mp3_output_filename
 
     @staticmethod
-    def generate_aac_from_mp4(mp4_input_filename, aac_output_filename, ffmpeg_preset_arg):
-        cmd = f"ffmpeg -i {mp4_input_filename} -vn -acodec copy {ffmpeg_preset_arg} {FileUtils.escape_file_name(aac_output_filename)}"
+    def generate_aac_from_mp4(
+        mp4_input_filename, aac_output_filename, ffmpeg_preset_arg
+    ):
+        cmd = f"ffmpeg -i {mp4_input_filename} -vn -acodec copy -shortest {ffmpeg_preset_arg} {FileUtils.escape_file_name(aac_output_filename)}"
         subprocess.check_output(cmd, shell=True)
 
         return aac_output_filename
