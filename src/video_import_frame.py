933782eb72722955045d27a0ced799ce59b24fd3
Hanxiong Shi <shihanxiong@gmail.com>
2023-05-18 23:18:32 -0400

    feat: combining play pause buttons (#104)
    * feat: combining play pause buttons
    
    * feat: ensure videos are imported before triggering play pause actions
    
    ---------
    
    Co-authored-by: Hanxiong Shi <hanxiong@havenlife.com>

--- src/video_import_frame.py
+++ src/video_import_frame.py
@@ -10,67 +10,62 @@ class VideoImportFrame(ttk.Frame):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         self.columnconfigure(2, weight=1)
-        self.columnconfigure(3, weight=1)
 
         self.video_import_button = ttk.Button(
-            self, text="Import videos", padding=(10), command=self.select_file)
+            self, text="Import videos", padding=(10), command=self.select_file
+        )
         self.video_import_button.grid(row=1, column=0, sticky="EW")
         self.clear_video_list_button = ttk.Button(
-            self, text="Clear video list", padding=(10), command=self.clear_video_list)
+            self, text="Clear video list", padding=(10), command=self.clear_video_list
+        )
         self.clear_video_list_button.grid(row=1, column=1, sticky="EW")
-        self.play_all_videos_button = ttk.Button(
-            self, text="Play all videos", state="disable", padding=(10), command=self.play_all)
-        self.play_all_videos_button.grid(row=1, column=2, sticky="EW")
-        self.pause_all_videos_button = ttk.Button(
-            self, text="Pause all videos", state="disable", padding=(10), command=self.pause_all)
-        self.pause_all_videos_button.grid(row=1, column=3, sticky="EW")
+        self.play_pause_videos_button = ttk.Button(
+            self,
+            text="Play all videos",
+            state="disable",
+            padding=(10),
+            command=self.play_pause,
+        )
+        self.play_pause_videos_button.grid(row=1, column=2, sticky="EW")
+        self.is_playing = False
 
     def refresh(self):
         if len(self.master.video_list) > 0:
-            self.set_buttons_status(
-                [self.play_all_videos_button, self.pause_all_videos_button], "enable")
+            self.play_pause_videos_button["state"] = "enable"
         else:
-            self.set_buttons_status(
-                [self.play_all_videos_button, self.pause_all_videos_button], "disable")
+            self.play_pause_videos_button["state"] = "disable"
 
         if len(self.master.video_list) == self.master.max_num_of_videos:
-            self.set_buttons_status([self.video_import_button], "disable")
+            self.video_import_button["state"] = "disable"
         else:
-            self.set_buttons_status([self.video_import_button], "enable")
+            self.video_import_button["state"] = "enable"
 
     def select_file(self):
-        filetypes = (
-            ('video files', '*.mp4'),
-            ('All files', '*.*')
-        )
+        filetypes = (("video files", "*.mp4"), ("All files", "*.*"))
 
         filenames = fd.askopenfilenames(
-            title='Open a file',
-            initialdir='/',
-            filetypes=filetypes)
+            title="Open a file", initialdir="/", filetypes=filetypes
+        )
 
         if filenames != None:
             self.master.video_list += filenames
             self.master.master.app_refresh()
             self.master.master.status_component.set_and_log_status(
-                f"Imported {filenames}")
+                f"Imported {filenames}"
+            )
 
     def clear_video_list(self):
         self.master.video_list = []
         self.master.master.app_refresh()
-        self.master.master.status_component.set_and_log_status(
-            "video list cleared")
+        self.master.master.status_component.set_and_log_status("video list cleared")
 
-    def set_buttons_status(self, buttons, status):
-        for button in buttons:
-            button["state"] = status
-
-    def play_all(self):
-        self.set_buttons_status([self.play_all_videos_button], "disable")
-        self.set_buttons_status([self.pause_all_videos_button], "enable")
-        self.master.video_renderer_component.play_all()
-
-    def pause_all(self):
-        self.set_buttons_status([self.pause_all_videos_button], "disable")
-        self.set_buttons_status([self.play_all_videos_button], "enable")
-        self.master.video_renderer_component.pause_all()
+    def play_pause(self):
+        if self.play_pause_videos_button["state"] == "enable":
+            if self.is_playing == True:
+                self.master.video_renderer_component.pause_all()
+                self.play_pause_videos_button.config(text="Play all videos")
+                self.is_playing = False
+            else:
+                self.master.video_renderer_component.play_all()
+                self.play_pause_videos_button.config(text="Pause all videos")
+                self.is_playing = True
