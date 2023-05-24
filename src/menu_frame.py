5a1dbe0aad360d4ba0fc7c8569a31c3e95c11db1
Hanxiong Shi <shihanxiong@gmail.com>
2023-05-24 01:57:23 -0400

    feat: added youtube timestamp export functionality (#108)
    * feat: added youtube timestamp export functionality
    
    * refactor: adjusted modal size in win32
    
    * refactor: better error handling
    
    ---------
    
    Co-authored-by: Hanxiong Shi <hanxiong@havenlife.com>

--- src/menu_frame.py
+++ src/menu_frame.py
@@ -7,6 +7,9 @@ from sys_utils import SysUtils
 
 # videos input
 class MenuFrame(ttk.Frame):
+    _MENU_CREATE_RANDOM_SEGMENTS = "create random segments"
+    _MENU_EXPORT_YOUTUBE_TIMESTAMP = "export YouTube timestamp"
+
     def __init__(self, container, **args):
         super().__init__(container, **args)
 
@@ -15,28 +18,40 @@ class MenuFrame(ttk.Frame):
         self.master.config(menu=menubar)
 
         # file
-        file_menu = Menu(menubar, font=self.master.default_font, tearoff="off")
+        self.file_menu = Menu(menubar, font=self.master.default_font, tearoff="off")
         # file_menu.add_separator()
-        file_menu.add_command(
+        self.file_menu.add_command(
             label="Exit",
             font=self.master.default_font,
             command=self.master.destroy,
         )
 
         # segment
-        segment_menu = Menu(
-            menubar, font=self.master.default_font, tearoff="off")
-        segment_menu.add_command(
-            label="create random segments",
+        self.segment_menu = Menu(menubar, font=self.master.default_font, tearoff="off")
+        self.segment_menu.add_command(
+            label=self._MENU_CREATE_RANDOM_SEGMENTS,
             font=self.master.default_font,
             command=self.show_create_random_segments_modal,
         )
+        self.segment_menu.add_command(
+            label=self._MENU_EXPORT_YOUTUBE_TIMESTAMP,
+            font=self.master.default_font,
+            command=self.show_export_youtube_timestamp_modal,
+            state="disabled",
+        )
 
-        menubar.add_cascade(label="File", menu=file_menu, underline=0)
-        menubar.add_cascade(label="Segment", menu=segment_menu, underline=0)
+        menubar.add_cascade(label="File", menu=self.file_menu, underline=0)
+        menubar.add_cascade(label="Segment", menu=self.segment_menu, underline=0)
 
     def refresh(self):
-        pass
+        if len(self.master.video_component.video_list) == 0:
+            self.segment_menu.entryconfig(
+                self._MENU_EXPORT_YOUTUBE_TIMESTAMP, state="disabled"
+            )
+        else:
+            self.segment_menu.entryconfig(
+                self._MENU_EXPORT_YOUTUBE_TIMESTAMP, state="active"
+            )
 
     def disable_menu_item(self, label):
         pass
@@ -46,7 +61,7 @@ class MenuFrame(ttk.Frame):
 
     def show_create_random_segments_modal(self):
         self.modal = Toplevel(self.master)
-        self.modal.title("create random segments")
+        self.modal.title(self._MENU_CREATE_RANDOM_SEGMENTS)
 
         if SysUtils.is_macos():
             self.modal.geometry("380x180")
@@ -65,37 +80,37 @@ class MenuFrame(ttk.Frame):
         number_of_videos_label = ttk.Label(
             self.modal, text="Number of videos", padding=(20)
         )
-        number_of_videos_label.grid(row=0, column=0, sticky="W")
-        self.number_of_videos_input = Entry(
-            self.modal, font=self.master.default_font)
-        self.number_of_videos_input.grid(row=0, column=1)
-
+        self.number_of_videos_input = Entry(self.modal, font=self.master.default_font)
         number_of_segments_label = ttk.Label(
             self.modal, text="Number of segments", padding=(20)
         )
-        number_of_segments_label.grid(row=1, column=0, sticky="W")
-        self.number_of_segments_input = Entry(
-            self.modal, font=self.master.default_font)
-        self.number_of_segments_input.grid(row=1, column=1)
-
+        self.number_of_segments_input = Entry(self.modal, font=self.master.default_font)
         minutes_per_segment_label = ttk.Label(
             self.modal, text="Minutes per segment", padding=(20)
         )
-        minutes_per_segment_label.grid(row=2, column=0, sticky="W")
         self.minutes_per_segment_input = Entry(
-            self.modal, font=self.master.default_font)
-        self.minutes_per_segment_input.grid(row=2, column=1)
-
+            self.modal, font=self.master.default_font
+        )
         confirm_button = ttk.Button(
             self.modal,
             text="Confirm",
             padding=(10),
             command=self.generate_segments,
         )
-        confirm_button.grid(row=3, column=0, sticky="E")
         cancel_button = ttk.Button(
             self.modal, text="Cancel", padding=(10), command=self.close_modal
         )
+
+        number_of_videos_label.grid(row=0, column=0, sticky="W")
+        self.number_of_videos_input.grid(row=0, column=1)
+
+        number_of_segments_label.grid(row=1, column=0, sticky="W")
+        self.number_of_segments_input.grid(row=1, column=1)
+
+        minutes_per_segment_label.grid(row=2, column=0, sticky="W")
+        self.minutes_per_segment_input.grid(row=2, column=1)
+
+        confirm_button.grid(row=3, column=0, sticky="E")
         cancel_button.grid(row=3, column=1, sticky="W")
 
     def generate_segments(self):
@@ -106,9 +121,71 @@ class MenuFrame(ttk.Frame):
                 num_videos=int(self.number_of_videos_input.get()),
             )
             self.master.timeline_component.timeline_text.insert(
-                END, random_segments_text)
-            self.master.status_component.set_and_log_status(
-                "random segments generated")
+                END, random_segments_text
+            )
+            self.master.status_component.set_and_log_status("random segments generated")
+            self.close_modal()
+        except Exception as err:
+            logging.error(f"{self.__class__.__name__}: {str(err)}")
+
+    def show_export_youtube_timestamp_modal(self):
+        self.modal = Toplevel(self.master)
+        self.modal.title(self._MENU_EXPORT_YOUTUBE_TIMESTAMP)
+
+        if SysUtils.is_macos():
+            self.modal.geometry("380x180")
+
+        # props
+        num_of_videos = len(self.master.video_component.video_list)
+        self.entries = []
+
+        self.modal.columnconfigure(0, weight=0)
+        self.modal.columnconfigure(1, weight=0)
+        self.modal.columnconfigure(2, weight=0)
+        for i in range(num_of_videos):
+            self.modal.rowconfigure(i, weight=0)
+            label = ttk.Label(self.modal, text=f"Label {i + 1}", padding=(20))
+            label.grid(row=i, column=0, sticky="EW")
+
+            entry = Entry(self.modal, font=self.master.default_font)
+            entry.grid(row=i, column=1, columnspan=2, sticky="EW", padx=(0, 40))
+            self.entries.append(entry)
+
+        confirm_button = ttk.Button(
+            self.modal,
+            text="Copy to clipboard",
+            padding=(10),
+            command=self.export_youtube_timestamp,
+        )
+        cancel_button = ttk.Button(
+            self.modal, text="Cancel", padding=(10), command=self.close_modal
+        )
+
+        confirm_button.grid(row=num_of_videos, column=1, sticky="EW", pady=(0, 20))
+        cancel_button.grid(
+            row=num_of_videos, column=2, sticky="EW", padx=(0, 40), pady=(0, 20)
+        )
+
+    def export_youtube_timestamp(self):
+        try:
+            timeline_text = self.master.timeline_component.get_timeline_text()
+
+            # get labels
+            labels = []
+            for entry in self.entries:
+                labels.append(entry.get())
+
+            # generate youtube timestamp
+            timestamp_text = TimelineUtils.generate_youtube_timestamp(
+                timeline_text, labels
+            )
+
+            # copy to clipboard
+            self.clipboard_clear()
+            self.clipboard_append(timestamp_text)
+            self.update()
+
+            # close modal
             self.close_modal()
         except Exception as err:
             logging.error(f"{self.__class__.__name__}: {str(err)}")
